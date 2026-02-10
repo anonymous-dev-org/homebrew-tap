5742133958d978b5b27b0cfb570dc1ca60e5291d
ZGFuZHk100 <hi@anonymous.dev>
2026-02-11 00:25:12 +0100

    zeroxzero: update to v1.2.3

--- Formula/zeroxzero.rb
+++ Formula/zeroxzero.rb
@@ -14,7 +14,7 @@ class Zeroxzero < Formula
       sha256 "17d612b3132bc4224c9eb81202d47912a1c43dc608f3294215a0abd8000cadc7"
 
       def install
-        bin.install "0x0"
+        bin.install "zeroxzero" => "0x0"
       end
     end
     if Hardware::CPU.arm?
@@ -22,7 +22,7 @@ class Zeroxzero < Formula
       sha256 "5e3b6f08dbbab7c1e3751b56bfcd4d1920c4a89aed57abd28591758c547cdb23"
 
       def install
-        bin.install "0x0"
+        bin.install "zeroxzero" => "0x0"
       end
     end
   end
@@ -32,14 +32,14 @@ class Zeroxzero < Formula
       url "https://github.com/anonymous-dev-org/0x0/releases/download/v1.2.3/0x0-linux-x64.tar.gz"
       sha256 "3432e36f001a4fc515f82b24c157ca5934bcebe6877fa9cd68588d49f907eaae"
       def install
-        bin.install "0x0"
+        bin.install "zeroxzero" => "0x0"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
       url "https://github.com/anonymous-dev-org/0x0/releases/download/v1.2.3/0x0-linux-arm64.tar.gz"
       sha256 "f304c82fb871796d31df51320066d23e3e3ffe90ab97719bd03b5d466e0cbda3"
       def install
-        bin.install "0x0"
+        bin.install "zeroxzero" => "0x0"
       end
     end
   end
