bd819fd582f715a9d1bf1546e36af1122469e306
ZGFuZHk100 <hi@anonymous.dev>
2026-03-01 12:48:09 +0100

    zeroxzero-git: update to v3.0.0

--- Formula/zeroxzero-git.rb
+++ Formula/zeroxzero-git.rb
@@ -4,20 +4,20 @@
 class ZeroxzeroGit < Formula
   desc "The AI coding agent built for the terminal."
   homepage "https://github.com/anonymous-dev-org/0x0"
-  version "2.4.1"
+  version "3.0.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v2.4.1/0x0-git-darwin-x64.zip"
-      sha256 "cf1dbedd6c0d0124db2f5f1de7315579117ed6e4a122775aa54baae46bfebc0e"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v3.0.0/0x0-git-darwin-x64.zip"
+      sha256 "39e7ce3a518e1914a4cb817c177067eaed6a6ab782c9ff4ebe433b6d34b2fb16"
 
       def install
         bin.install "0x0-git"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v2.4.1/0x0-git-darwin-arm64.zip"
-      sha256 "56d7aac61a670140bbfab9ef13c5364edbaf75e718a06b7d38fe7b6cecafa9ad"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v3.0.0/0x0-git-darwin-arm64.zip"
+      sha256 "e30467cf43a9b3e78b127d98a1d51f04eb191e9615777ead3ba1ffa45475214d"
 
       def install
         bin.install "0x0-git"
@@ -27,15 +27,15 @@ class ZeroxzeroGit < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v2.4.1/0x0-git-linux-x64.tar.gz"
-      sha256 "8a3b5e0ef3d5238684499de249d3cd91a80739eb2c6db5a5386d4f1a0416f479"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v3.0.0/0x0-git-linux-x64.tar.gz"
+      sha256 "56255615ec8a021dd4460922d831aaa7e17d6255caec6a36bfddabae0ffbc6ad"
       def install
         bin.install "0x0-git"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v2.4.1/0x0-git-linux-arm64.tar.gz"
-      sha256 "a6973b8e1c90d184bbcceb56a496fb95c9ea508d24e8c676495acbf5317dc866"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-git-v3.0.0/0x0-git-linux-arm64.tar.gz"
+      sha256 "4b138c4409bc2afe6f9274143d30b6c477c00bad9db6fec5422f23e67887ce57"
       def install
         bin.install "0x0-git"
       end
