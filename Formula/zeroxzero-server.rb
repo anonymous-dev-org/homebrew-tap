6acd4b8ba3e7b4a4ba493023cc2102511d5cdb7c
ZGFuZHk100 <hi@anonymous.dev>
2026-03-07 17:38:33 +0100

    zeroxzero-server: update to v2.5.1

--- Formula/zeroxzero-server.rb
+++ Formula/zeroxzero-server.rb
@@ -4,20 +4,20 @@
 class ZeroxzeroServer < Formula
   desc "The AI coding agent daemon."
   homepage "https://github.com/anonymous-dev-org/0x0"
-  version "2.5.0"
+  version "2.5.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.0/0x0-server-darwin-x64.zip"
-      sha256 "457eb0ae0126b65ad835d6c26d47698ab971242632294afaaa04fefa45900126"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.1/0x0-server-darwin-x64.zip"
+      sha256 "48a6345a1ca3bd38142b778cd6921c2a5388ed26d17dbe3d1dd69e3db809296b"
 
       def install
         bin.install "0x0-server"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.0/0x0-server-darwin-arm64.zip"
-      sha256 "b317302dc7854b0ca598a15f1e17728324e5d0b0096a8475e7ad8ab48fd2668e"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.1/0x0-server-darwin-arm64.zip"
+      sha256 "ca296fb979e31b176a5cb5a0ccdfaf41fe8caf270515ec9b5d5ce6578b0b9c72"
 
       def install
         bin.install "0x0-server"
@@ -27,15 +27,15 @@ class ZeroxzeroServer < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.0/0x0-server-linux-x64.tar.gz"
-      sha256 "4370c47c0650e59fb810af77fc7576ba83aa7ad8b41d90474c0dfdd0368f5e6f"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.1/0x0-server-linux-x64.tar.gz"
+      sha256 "27d70314914b2855f79adafdfbb310ca150116398e6522686ba0c377c86cf1ec"
       def install
         bin.install "0x0-server"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.0/0x0-server-linux-arm64.tar.gz"
-      sha256 "bd50a85d6344421462084ca0fe78f3342e860f63a7faa319ce52a86a0b67c49f"
+      url "https://github.com/anonymous-dev-org/0x0/releases/download/0x0-server-v2.5.1/0x0-server-linux-arm64.tar.gz"
+      sha256 "56903ffcf2fd72880bbdc66ae936850f41041cd13a4c359a4be864e3176ccc8e"
       def install
         bin.install "0x0-server"
       end
