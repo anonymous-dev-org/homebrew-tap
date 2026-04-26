7587bee8e712b386f8a03e9bba7b969ce81345be
p78tr4 <p78tr4@MacBookPro-2.homenet.telecomitalia.it>
2026-04-26 16:12:57 +0200

    Handle arm64 Homebrew formula detection

--- Formula/zeroxzero-server.rb
+++ Formula/zeroxzero-server.rb
@@ -6,28 +6,18 @@ class ZeroxzeroServer < Formula
   homepage "https://github.com/anonymous-dev-org/0x0"
   version "0.1.0"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-darwin-arm64.zip"
-      sha256 "8346a69ce3497ddc578c2c46262fd758a00f8540dc42121f99522648d234af58"
-    end
-
-    on_intel do
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-darwin-x64.zip"
-      sha256 "5c67f4601149063027fbb7b1f33766dfde74461f95fbb495f71f3b8eb3973ef5"
-    end
-  end
-
-  on_linux do
-    on_arm do
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-linux-arm64.tar.gz"
-      sha256 "403df4dfe2cf1dca2ceca80cb10aace2580435e5fe7d5e090b4b200a3756f8ff"
-    end
-
-    on_intel do
-      url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-linux-x64.tar.gz"
-      sha256 "31788d06ecc40fac99222dea041c1f81674f5770a3b7be26bcbc65d94adca73e"
-    end
+  if OS.mac? && (Hardware::CPU.arm? || RUBY_PLATFORM.include?("arm64"))
+    url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-darwin-arm64.zip"
+    sha256 "8346a69ce3497ddc578c2c46262fd758a00f8540dc42121f99522648d234af58"
+  elsif OS.mac?
+    url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-darwin-x64.zip"
+    sha256 "5c67f4601149063027fbb7b1f33766dfde74461f95fbb495f71f3b8eb3973ef5"
+  elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-linux-arm64.tar.gz"
+    sha256 "403df4dfe2cf1dca2ceca80cb10aace2580435e5fe7d5e090b4b200a3756f8ff"
+  elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+    url "https://github.com/anonymous-dev-org/0x0/releases/download/v0.1.0/0x0-linux-x64.tar.gz"
+    sha256 "31788d06ecc40fac99222dea041c1f81674f5770a3b7be26bcbc65d94adca73e"
   end
 
   def install
